53264e1306257c21733e24cb959156acd5129eda
Dashnyam <lord_dashka277@yahoo.com>
2020-05-16 20:34:38 +0800

    headeree buren hiij duusgav

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,6 +1,9 @@
 import "../styles/styles.css";
+import MobileMenu from "./modules/MobileMenu";
 
+let mobileMenu = new MobileMenu();
+
+// webpack - ыг watch хийж байгаа
 if ((module, hot)) {
   module.hot.accept();
-  2;
 }
