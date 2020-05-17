a01668ca2802714bc8eb1c80bb2ae1c0acf11ccf
Dashnyam <lord_dashka277@yahoo.com>
2020-05-17 16:00:52 +0800

    Reavel featureiig hiij duusgav

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,9 +1,13 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 85);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 
 let mobileMenu = new MobileMenu();
 
 // webpack - ыг watch хийж байгаа
-if ((module, hot)) {
+if (module.hot) {
   module.hot.accept();
 }
